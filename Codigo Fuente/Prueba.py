c12f197aa80daf5ad9b0bc109342a2f05ba8b250
Mariobpv22 <114129410+Mariobpv22@users.noreply.github.com>
2023-06-30 10:08:07 -0500

    Update Final Prueba.py

--- Codigo Fuente/Prueba.py
+++ Codigo Fuente/Prueba.py
@@ -4,6 +4,30 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import os
 
+# Implementación del algoritmo UFDS
+class UFDS:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        
+        if root_x != root_y:
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+
 # Cargar los datos del archivo CSV
 canciones_df = pd.read_csv('canciones.csv')
 nodos_df = pd.read_csv('canciones_nodos.csv')
@@ -31,21 +55,48 @@ for index, row in nodos_df.iterrows():
 # Crear el grafo a partir de los datos de los nodos y las aristas
 grafo = nx.from_pandas_edgelist(aristas_df, 'Source', 'Target', ['Weight', 'Label', 'Type'])
 
-# Función para buscar canciones relacionadas
+# Función para buscar canciones relacionadas utilizando UFDS
 def buscar_canciones(relacion):
     resultados = []
+    
+    # Crear instancias de UFDS para agrupar canciones por artista y álbum
+    ufds_artista = UFDS(len(canciones_df))
+    ufds_album = UFDS(len(canciones_df))
+
     for index, row in canciones_df.iterrows():
         cancion = row['Canción']
         artista = row['Artista']
         album = row['Álbum']
+        
+        # Realizar uniones basadas en el nombre de canción, artista y álbum
         if relacion.lower() in cancion.lower() or relacion.lower() in artista.lower() or relacion.lower() in album.lower():
             resultados.append((cancion, artista, album))
 
-    return resultados
+            # Realizar uniones basadas en el índice de la canción
+            ufds_artista.union(index, 0)
+            ufds_album.union(index, 0)
+
+    # Obtener los conjuntos disjuntos finales
+    conjuntos_artista = []
+    conjuntos_album = []
+    for i in range(len(canciones_df)):
+        if ufds_artista.find(i) == 0:
+            conjunto_artista = set()
+            conjunto_album = set()
+            for j in range(len(canciones_df)):
+                if ufds_artista.find(j) == 0 and ufds_artista.find(j) == ufds_artista.find(i):
+                    conjunto_artista.add(j)
+                if ufds_album.find(j) == 0 and ufds_album.find(j) == ufds_album.find(i):
+                    conjunto_album.add(j)
+            conjuntos_artista.append(conjunto_artista)
+            conjuntos_album.append(conjunto_album)
+
+    return resultados, conjuntos_artista, conjuntos_album
+
 # Función para mostrar los resultados de la búsqueda en la interfaz gráfica
 def mostrar_resultados():
     relacion = entrada.get()
-    canciones_relacionadas = buscar_canciones(relacion)
+    canciones_relacionadas, conjuntos_artista, conjuntos_album = buscar_canciones(relacion)
 
     for widget in resultados_frame.winfo_children():
         widget.destroy()
